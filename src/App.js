f1decf73e09b37ce107f40501683275031c2f7c1
gaurav garg <gauravgarg@gauravs-MacBook-Pro.local>
2018-10-05 18:34:18 +0530

    sdk integration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import domtoimage from 'dom-to-image';
+import { Portal } from 'react-portal';
 
 
 function capture() {
     debugger;
-    domtoimage.toPng(document.getElementById('root'))
+    domtoimage.toPng(document.getElementById('badge'), {style: {visibility: 'visible'}})
         .then(function (dataUrl) {
             var img = new Image();
             var c = document.getElementById("myCanvas");
@@ -32,13 +33,23 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {path: logo}
-        capture();
-    }
 
+    }
+componentDidMount(){
+        capture()
+}
     render() {
         return (
             <div className="App" >
-                <img className='bg' style={{flex:1,height:'100vh',width:'100vw'}} src={'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350'} />
+                <div> This text is portaled into San Francisco!</div>
+                <Portal node={document && document.getElementById('badge')}>
+                    <img className='bg' style={{flex:1,height:'30vh',width:'30vw'}} src={'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350'} />
+
+                    <div> This text is portaled into San Francisco!</div>
+                </Portal>
+                <div> This text is portaled into San Francisco!</div>
+                <div> This text is portaled into San Francisco!</div>
+
             </div>
         );
     }
